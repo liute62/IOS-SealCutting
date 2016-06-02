23c2512a65e50b865cf2af0c9dc5e85612256d5a
liute62 <liute62@sina.cn>
2016-06-02 23:42:06 +0800

    Add icon set first time, and update template selected view

---
+++ SealCutting/DetailAdjustViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailAdjustViewController.swift
+//  SealCutting
+//
+//  Created by 刘昊东 on 16/6/2.
+//  Copyright © 2016年 haodong, liu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailAdjust : UIViewController{
+    
+}
--- SealCutting/FirstInputViewController.swift
+++ SealCutting/FirstInputViewController.swift
@@ -14,4 +14,21 @@ class FirstInputViewController: UITableViewController{
     @IBAction func toggleMenu(sender: AnyObject) {
         NSNotificationCenter.defaultCenter().postNotificationName("toggleMenu", object: nil)
     }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        //根据storyboard获取controller
+        let sb = UIStoryboard(name:"TemplateSelected", bundle: nil)
+        let templateSelected = sb.instantiateViewControllerWithIdentifier("TemplateSelected") as! TemplateSelectedViewController
+        self.navigationController?.pushViewController(templateSelected, animated: true)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == ""{
+            
+        }
+    }
+    
+    func prepareForPass(indexPath:NSIndexPath) {
+        self.tableView.deselectRowAtIndexPath(indexPath, animated: false)
+    }
 }
